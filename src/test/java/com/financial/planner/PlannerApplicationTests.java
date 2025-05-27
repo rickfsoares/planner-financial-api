4f8704826d52aafbda91c7b5ba224e435c73360e
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-05-27 16:36:46 -0300

    added initial project

---
+++ src/test/java/com/financial/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
